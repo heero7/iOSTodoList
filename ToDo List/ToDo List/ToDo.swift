78b1119c0fdf450fa99be8113f6d3009b60f3b4f
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-08 23:21:54 -0600

    Created todo class to make todo objects for the users list

---
+++ ToDo List/ToDo List/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Kevin Mudiandambo on 11/8/17.
+//  Copyright © 2017 Kevin Mudiandambo. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+    
+}
--- ToDo List/ToDo List/TodoTableViewController.swift
+++ ToDo List/ToDo List/TodoTableViewController.swift
@@ -10,86 +10,53 @@ import UIKit
 
 class TodoTableViewController: UITableViewController {
 
+    var todoList : [ToDo] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        todoList = createTestTodos()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return todoList.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let todo = todoList[indexPath.row]  //whatever index/cell number we're looking at, get that                   //  todo item
+        
+        if todo.important {
+            cell.textLabel?.text = "❗" + todo.name
+        } else {
+            cell.textLabel?.text = todo.name
+        }
+        
         return cell
     }
-    */
-
+    
+    //MARK: Testing vars and functions
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+     Creates a dummy list of todo items
+     for testing purposes
+     */
+    func createTestTodos() -> [ToDo] {
+        let eggs = ToDo()
+        eggs.name = "Buy eggs"
+        eggs.important = true
+        
+        let fruit = ToDo()
+        fruit.name = "Get apples, strawberries, and grapes"
+        fruit.important = true
+        
+        let napkins = ToDo()
+        napkins.name = "Get napkins for kitchen table"
+        napkins.important = false
+        
+        return [eggs, fruit, napkins]
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
