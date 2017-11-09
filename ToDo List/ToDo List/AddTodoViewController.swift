9d89be4d95a72225b369cbe8655cf1831802b13c
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-08 23:34:16 -0600

    Added ViewController for adding a todo item

---
+++ ToDo List/ToDo List/AddTodoViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddTodoViewController.swift
+//  ToDo List
+//
+//  Created by Kevin Mudiandambo on 11/8/17.
+//  Copyright © 2017 Kevin Mudiandambo. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importanceSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addToDoItem(_ sender: Any) {
+        
+    }
+}
