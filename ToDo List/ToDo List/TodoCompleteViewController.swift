56aa28d6c75109f38566108a02b8837e9fb38b81
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2017-11-12 14:07:35 -0600

    Finished functionality for deleting an item to the list

--- ToDo List/ToDo List/TodoCompleteViewController.swift
+++ ToDo List/ToDo List/TodoCompleteViewController.swift
@@ -22,9 +22,34 @@ class TodoCompleteViewController: UIViewController {
     
     
     @IBAction func completeTodo(_ sender: Any) {
+        var index = 0
+        for todo in previousVC.todoList {
+            if todo.name == selectedTodo.name {
+                previousVC.todoList.remove(at: index)
+                reloadDataInViewController()
+                moveBackToMainView()
+                break
+            }
+           index += 1
+        }
     }
     
     private func setup() {
         titleLabel.text = selectedTodo.name
+    }
+    
+    /*
+     Reload the data in the ViewController so
+     we gat the most current to do data.
+     */
+    private func reloadDataInViewController(){
+        previousVC.tableView.reloadData()
+    }
+    
+    /*
+     Moves back to the main view
+     */
+    private func moveBackToMainView(){
+        navigationController?.popViewController(animated: true)
     }
 }
